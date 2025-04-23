39be66d5dd2590b8099291dcf22a760d5d2385b5
Morgana <morganaschnneider@gmail.com>
2025-04-23 04:00:11 -0300

    Add files via upload

---
+++ dev/curso/php4noobs/comments.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Aula de comentários
+ *
+ * Nessa aula eu estou aprendendo a diferença entre comentários e linha única
+ * e comentários de multiplas linhas, tambémo conhecido como DocBlock
+ */
+
+// Aqui abaixo está uma string falando bobeiras
+
+echo "Ola Twich";
---
+++ dev/curso/php4noobs/output.php
@@ -0,0 +1,16 @@
+<?php
+echo "Ola Twich"."-" . 123 . PHP_EOL;
+/**
+ *Aula sobre funções de saída
+
+ *Nessa aula vou aprender como utilizar funções para conseguir debugar
+ * e visualizar  o que está acontecendo no código.
+ *
+ * As funções a serem utilizadas: echo, var_dump
+ *
+ * Constantes a serem utilizadas: PHP_EOL
+ */
+var_dump("djsajdasj");
+var_dump(true);
+var_dump(12.2222);
+var_dump([1,2,3,4]);
