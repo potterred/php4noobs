40a533c085bde6d80a00aa43a9f5b3d820052340
Morgana <morganaschnneider@gmail.com>
2025-04-23 04:16:32 -0300

    Add files via upload

---
+++ dev/curso/php4noobs/dataTypes.php
@@ -0,0 +1,11 @@
+<?php
+/** Aula sobre Tipos de Dados */
+
+var_dump('eae galera da twich');
+var_dump(true);
+var_dump(false);
+var_dump( 302130);
+var_dump(190239201.30921);
+
+$dados =['name'=>'Morgana','29','minha casa'];
+var_dump($dados['name']);
