79f3b1a9eb23ccf29409e27c3f3d2982a03dbd1c
Morgana <morganaschnneider@gmail.com>
2025-04-30 11:27:01 -0300

    Add files via upload

---
+++ dev/curso/php4noobs/atributtionOperatores.php
@@ -0,0 +1,23 @@
+<?php
+/** Aula sobre operadores  de Atribuição ...*/
+
+$itemA= 50;
+$itemA+= 25;
+
+echo $itemA . PHP_EOL;
+
+$itemA -= 25;
+
+echo $itemA . PHP_EOL;
+
+$itemA /= 2;
+
+echo $itemA . PHP_EOL;
+
+$itemA *= $itemA;
+
+echo $itemA . PHP_EOL;
+
+$itemA %= 2;
+
+echo $itemA . PHP_EOL;
